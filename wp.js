7b31d9d8c1b3cd47d3e32c0c598f7cb26e324572
arbrebinaire <github@arbrebinaire.com>
2021-06-06 13:31:53 -0400

    Update wp.js

--- wp.js
+++ wp.js
@@ -1,5 +1,2 @@
-const html = document.getElementsByTagName('html');
-const body = document.getElementsByTagName('body');
-
-html.removeAttribute('style');
-body.removeAttribute('style');
+$('html').removeAttr('style');
+$('body').removeAttr('style');
