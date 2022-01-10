4e10c4d116b9245afca45b5458db79c951cd3054
arbrebinaire <github@arbrebinaire.com>
2022-01-09 21:07:11 -0500

    Update nyt.js

--- nyt.js
+++ nyt.js
@@ -1,2 +1,2 @@
-document.getElementById('app').children[0].children[0].children[2].remove().children[0].children[0].setAttribute('style', 'position:relative;');
-document.getElementById('app').children[0].children[0].children[2].remove().children[0].children[0].children[2].remove();
+document.getElementById('app').children[0].children[0].setAttribute('style', 'position:relative;');
+document.getElementById('app').children[0].children[0].children[2].remove();
